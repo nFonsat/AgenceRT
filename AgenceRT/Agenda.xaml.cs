aad590f8a941183aff588637ea8c46638117738f
Fonsat Nicolas <fonsat.nicolas@gmail.com>
2015-03-06 10:55:15 +0100

    add calenar in pplication

--- AgenceRT/Agenda.xaml.cs
+++ AgenceRT/Agenda.xaml.cs
@@ -29,19 +29,17 @@ namespace AgenceRT
     public sealed partial class Agenda : AgenceRT.Common.LayoutAwarePage
     {
         private ObservableCollection<AgendaEntite> _agendasAffiches = new ObservableCollection<AgendaEntite>();
-
         public ObservableCollection<AgendaEntite> AgendasAffiches
         {
             get { return _agendasAffiches; }
             set { _agendasAffiches = value; }
         }
 
-        private Calendar _calendar = new Calendar();
-
-        public Calendar Calendar
+        private ObservableCollection<AgendaEntite> _rdvCurrentDay = new ObservableCollection<AgendaEntite>();
+        public ObservableCollection<AgendaEntite> RDVCurrentDay
         {
-            get { return _calendar; }
-            set { _calendar = value; }
+            get { return _rdvCurrentDay; }
+            set { _rdvCurrentDay = value; }
         }
 
         public Agenda()
@@ -49,6 +47,11 @@ namespace AgenceRT
             this.InitializeComponent();
 
             this.DataContext = this;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ChargerAgendas();
         }
 
         /// <summary>
@@ -74,29 +77,143 @@ namespace AgenceRT
         {
         }
 
-        private async void ChargerBiens()
+        private async void ChargerAgendas()
         {
             _agendasAffiches.Clear();
 
             ObservableCollection<AgendaDTO> agendas;
             AgenceWebServicesClient ws = new AgenceWebServicesClient();
 
-            agendas = await ws.ChargerListeRendezVousAsync();
+            try
+            {
+                agendas = await ws.ChargerListeRendezVousAsync();
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("Erreur Charger Agenda : " + e.Message);
+                return;
+            }
+            
 
             foreach (AgendaDTO agenda in agendas)
             {
-                AgendaEntite agendaEntite = new AgendaEntite { 
-                                                IdAgenda = agenda.IdAgenda, 
-                                                Date = agenda.Date,
-                                                Description = agenda.Description, 
-                                                Titre = agenda.Titre, 
-                                                Agent = new AgentEntite(),
-                                                Prospect = new ProspectEntite(), 
-                                                Annonce = new AnnonceEntite()
-                                            };
+                AgentEntite agent = new AgentEntite();
+                try
+                {
+                    agent.Login = agenda.Agent.Login;
+                    agent.MotDePasse = agenda.Agent.MotDePasse;
+                }
+                catch (Exception e)
+                {
+                    agent = new AgentEntite();
+                    System.Diagnostics.Debug.WriteLine("Error AgentEntite : " + e.Message);
+                }
+
+                ProspectEntite prospect = new ProspectEntite();
+                try
+                {
+                    prospect.DateContact = agenda.Prospect.DateContact;
+                }
+                catch (Exception e)
+                {
+                    prospect = new ProspectEntite();
+                    System.Diagnostics.Debug.WriteLine("Error ProspectEntite : " + e.Message);
+                }
+
+                ProprietaireEntite proprietaire = new ProprietaireEntite();
+                try
+                {
+                    proprietaire.Adresse = agenda.Annonce.Bien.Proprietaire.Adresse;
+                }
+                catch (Exception e)
+                {
+                    proprietaire = new ProprietaireEntite();
+                    System.Diagnostics.Debug.WriteLine("Error ProprietaireEntite : " + e.Message);
+                }
+
+                BienEntite bien = new BienEntite();
+                try
+                {
+                    bien.Adresse = agenda.Annonce.Bien.Adresse;
+                    bien.Description = agenda.Annonce.Bien.Description;
+                    bien.IdBien = agenda.Annonce.Bien.IdBien;
+                    bien.IdTypeBien = agenda.Annonce.Bien.IdTypeBien;
+                    bien.Latitude = agenda.Annonce.Bien.Latitude;
+                    bien.LibelleType = agenda.Annonce.Bien.LibelleType;
+                    bien.Longitude = agenda.Annonce.Bien.Longitude;
+                    bien.Proprietaire = proprietaire;
+                    bien.Titre = agenda.Annonce.Bien.Titre;
+                }
+                catch (Exception e)
+                {
+                    bien = new BienEntite();
+                    System.Diagnostics.Debug.WriteLine("Error BienEntite : " + e.Message);
+                }
+
+                AnnonceEntite annonce = new AnnonceEntite();
+                try
+                {
+                    annonce.IdAnnonce = agenda.Annonce.IdAnnonce;
+                    annonce.Titre = agenda.Annonce.Titre;
+                    annonce.Bien = bien;
+                    annonce.Texte = agenda.Annonce.Texte;
+                    annonce.Prix = agenda.Annonce.Prix;
+                }
+                catch (Exception e)
+                {
+                    annonce = new AnnonceEntite();
+                    System.Diagnostics.Debug.WriteLine("Error AnnonceEntite : " + e.Message);
+                }
+
+                AgendaEntite agendaEntite = new AgendaEntite 
+                { 
+                    IdAgenda = agenda.IdAgenda, 
+                    Date = agenda.Date,
+                    Description = agenda.Description, 
+                    Titre = agenda.Titre,
+                    Agent = agent,
+                    Prospect = prospect, 
+                    Annonce = new AnnonceEntite()
+                };
 
                 _agendasAffiches.Add(agendaEntite);
             }
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Calendar cal = (Calendar)sender;
+            
+            System.Diagnostics.Debug.WriteLine("Date Selected");
+            System.Diagnostics.Debug.WriteLine(_agendasAffiches.Count);
+
+            DateTime dateSelected = (DateTime)e.AddedItems[0];
+            DateTime dateMax = new DateTime(dateSelected.Year, dateSelected.Month, dateSelected.Day + 1);
+
+            _rdvCurrentDay.Clear();
+            foreach(AgendaEntite rdv in _agendasAffiches)
+            {
+                if ((rdv.Date > dateSelected) && (rdv.Date < dateMax))
+                {
+                    _rdvCurrentDay.Add(rdv);
+                }
+            }
+            
+        }
+
+        private void Calendar_DisplayDateChanged(object sender, CalendarDateChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Date Changed");
+            DateTime date = e.AddedDate.Value;
+            System.Diagnostics.Debug.WriteLine(date.Month + "/" + date.Year);
+            
+            
+        }
+
+        private void calendar_DisplayModeChanged(object sender, CalendarModeChangedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Mode Changed");
+            System.Diagnostics.Debug.WriteLine(e.NewMode);
         }
     }
 }
